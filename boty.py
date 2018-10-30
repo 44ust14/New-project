d21e12c5604415d8dc8591156ffa6f05bd5b4084
Lajob <ustym.hanyk@ukr.net>
2018-10-30 20:26:09 +0200

    heroku deploy

--- boty.py
+++ boty.py
@@ -21,25 +21,6 @@ geocoder = OpenCageGeocode(key)
 #         longitude = result[0]['geometry']['lng']
 #         latitude  = result[0]["geometry"]["lat"]
 # print(latitude,longitude)
-def set_proxy(url, basic_auth=None):
-    """
-    Access Bot API through a proxy.
-
-    :param url: proxy URL
-    :param basic_auth: 2-tuple ``('username', 'password')``
-    """
-    global _pools, _onetime_pool_spec
-    if not url:
-        _pools['default'] = urllib3.PoolManager(**_default_pool_params)
-        _onetime_pool_spec = (urllib3.PoolManager, _onetime_pool_params)
-    elif basic_auth:
-        h = urllib3.make_headers(proxy_basic_auth=':'.join(basic_auth))
-        _pools['default'] = urllib3.ProxyManager(url, proxy_headers=h, **_default_pool_params)
-        _onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=url, proxy_headers=h, **_onetime_pool_params))
-    else:
-        _pools['default'] = urllib3.ProxyManager(url, **_default_pool_params)
-        _onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=url, **_onetime_pool_params))
-
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(msg)
